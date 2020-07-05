bdc0191e9e30fed175b1f3d3b85e2b0b22a6a975
patricoferris <patrickferris17@gmail.com>
2020-07-05 17:15:21 +0100

    Add prelim fb

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,5 +70,18 @@ module.exports = {
     },
     `gatsby-plugin-typescript`,
     `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/images/chip.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,7 @@ import locales from '../../config/i18n'
 import { Paper } from '@material-ui/core'
 import LocaleContext from '../context/LocaleProvider'
 import Foot from '../components/Foot'
+import { withPrefix } from 'gatsby'
 const pages = ['home', 'about', 'partners', 'media', 'contact']
 
 interface LayoutProps {
@@ -26,6 +27,7 @@ const Layout: React.SFC<LayoutProps> = ({ children }) => {
           >
             <Helmet>
               <script defer src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+              <script src={withPrefix('firebase.js')} type="text/javascript" />
               <meta charSet="utf-8" />
               <meta
                 name="description"
