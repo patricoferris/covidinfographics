67c4354d4d784556f9c9c13e444b3bea2fb23e39
patricoferris <patrickferris17@gmail.com>
2020-07-25 12:18:56 +0100

    Redesign parte dos

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -7,7 +7,8 @@ import { Layout } from '../components/Layout'
 import localised from '../utils/text'
 
 import BackgroundImage from 'gatsby-background-image'
-import { Typography } from '@material-ui/core'
+import { Typography, Button } from '@material-ui/core'
+import LocalizedLink from '../components/LocalizedLink'
 
 type IndexContents = Record<string, unknown>
 
@@ -35,6 +36,12 @@ const useStyles = makeStyles((theme) => ({
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
+  aboutImage: {
+    width: '100%',
+    height: '500px',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
   textMain: {
     backgroundColor: 'rgba(0, 0, 0, 0.55)',
     padding: '0.5rem',
@@ -42,13 +49,23 @@ const useStyles = makeStyles((theme) => ({
     color: 'white',
     borderRadius: '2px',
   },
+  aboutText: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20%',
+    alignContent: 'center',
+    backgroundColor: '#E2EEC2',
+    height: '500px',
+  },
 }))
 
 const Index: React.SFC<IndexProps> = ({ pageContext: { local, english } }) => {
   const classes = useStyles()
   return (
     <Layout>
-      <Grid container>
+      <Grid container spacing={5}>
         <Grid item sm={12} md={12}>
           <BackgroundImage
             className={classes.bgImage}
@@ -62,14 +79,21 @@ const Index: React.SFC<IndexProps> = ({ pageContext: { local, english } }) => {
             </div>
           </BackgroundImage>
         </Grid>
-        <Grid item sm={12} md={12}>
-          <Typography style={{ fontWeight: '700', color: 'white' }} variant="h1">
-            {localised(local, english, 'title')}
-          </Typography>
-          <Typography style={{ color: 'white' }} variant="h1">
-            {localised(local, english, 'message')}
-          </Typography>
+        <Grid style={{ height: '40px' }} item sm={false} md={12} />
+        <Grid item sm={false} md={1} />
+        <Grid item sm={12} md={5}>
+          <div className={classes.aboutText}>
+            <Typography style={{ marginBottom: '1.2rem' }} variant="h3">{localised(local, english, 'aboutTitle')}</Typography>
+            <Typography style={{ marginBottom: '1.2rem' }} variant="body1">{localised(local, english, 'aboutMessage')}</Typography>
+            <Button variant="outlined">
+              <LocalizedLink to={'/about'}>About</LocalizedLink>
+            </Button>
+          </div>
         </Grid>
+        <Grid item sm={12} md={5}>
+          <BackgroundImage className={classes.aboutImage} fluid={localised(local, english, 'titleBgImg').childImageSharp.fluid} />
+        </Grid>
+        <Grid style={{ height: '100px' }} item sm={false} md={12} />
       </Grid>
     </Layout>
   )
